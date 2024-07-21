57ad68033582c3d1d05261da52653226c29ab85a
arpita-belsare <arpitabelsare22@navgurukul.org>
2024-07-21 13:29:44 +0530

    cart css

--- src/Page's/CartPage.jsx
+++ src/Page's/CartPage.jsx
@@ -9,19 +9,27 @@ const handleClearCart = ()=>{
     clearCart();
 }
   return (
-  <div className="m">
-    <h1>CartItems - ({cartItems.length})</h1>
+  <div className="m"> 
+    <h1>CartItems - ({cartItems.length})</h1><br />
     <button onClick={handleClearCart} >Clear Cart</button>
+    <div className="Cartwrap">
     {
         cartItems?.map((item, index) => (
+            <div className="one-cart">
             <div key={index} >
-                <img src={item.image} alt={item.title} />
-                <h3> {item.title} </h3>
-                <p> {item.price} </p>
+                  <img src={item.image} alt={item.title} />
+             <div className="product-details">
+             <h3> {item.title} </h3>
+             <span className="strik">$400</span>
+                <span>  ${item.price}</span>
                 <p> {item.description}</p>
+                <button>Buy Now</button>
+             </div>
+              </div>
             </div>
         ))
     }
+    </div>
   </div>
   )
 }
